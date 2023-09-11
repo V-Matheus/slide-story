c276ec6b615332d041478e4099e25e5f1ef0b2fe
Victor Matheus <victormatheus507@gmail.com>
2023-09-11 15:03:37 -0300

    estrutura slide

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,8 @@
-"use strict";
+import Slide from "./slide.js";
+const container = document.getElementById("slide");
+const elements = document.getElementById("slide-elements");
+const constrols = document.getElementById("slide-controls");
+if (container && elements && constrols && elements.children.length) {
+    new Slide(container, Array.from(elements.children), constrols, 3000);
+}
 //# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,9 @@
+import Slide from "./slide.js";
+
+const container = document.getElementById("slide");
+const elements = document.getElementById("slide-elements");
+const constrols = document.getElementById("slide-controls");
+
+if (container && elements && constrols && elements.children.length) {
+  new Slide(container, Array.from(elements.children), constrols, 3000);
+}
